e3ed5e48aac3ba92fa7615510070ff34544b733a
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-01-05 09:43:41 +0700

    create ClassDAO

---
+++ src/main/java/com/atcollabo/hackathon/classdojo/dao/ClassDAO.java
@@ -0,0 +1,57 @@
+package com.atcollabo.hackathon.classdojo.dao;
+
+import com.atcollabo.hackathon.classdojo.entity.Class;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ClassDAO {
+    private final EntityManager entityManager;
+
+    // save
+    @Transactional
+    public void save (Class _class){
+        if(_class.getId() == null){ // new class not in database we need to persist/save/insert it
+            entityManager.persist(_class);
+        }
+        else { // if the class already exists in the database we need to update it
+            entityManager.merge(_class);
+        }
+    }
+
+
+    // findOne
+    public Class findOne(Long id){
+        return entityManager.find(Class.class, id); // find(entity type, primary key)
+    }
+
+
+    // findAll
+    public List<Class> findAll(){
+        return entityManager.createQuery("select c from Class c", Class.class)
+                .getResultList();
+    }
+
+
+    // findByCode
+    public List<Class> findByCode(String code){
+        return entityManager.createQuery("select c from Class c where c.code = :code", Class.class)
+                .setParameter("code", code)
+                .getResultList();
+    }
+
+
+    // Delete
+    @Transactional
+    public void delete(Long id){
+        Class _class = entityManager.find(Class.class, id);
+        entityManager.remove(_class);
+    }
+
+
+}
